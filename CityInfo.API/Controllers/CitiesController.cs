ed91e7e2716d29daf189962555ebedcfbee53a9a
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-07-22 22:03:33 +0200

    first commit

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController] // this itsn't strictly necessary, but it adds some useful features
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id = 1, Name = "Mechelen" },
+                    new {id = 2, Name = "Antwerpen" },
+                    new {id = 3, Name = "Brussels" }
+                });
+        }
+    }
+}
